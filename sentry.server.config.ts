20f93f70d464397d7aab4ca51218a0fa007f2e2c
echelonsystemsgd-deep <echelonsystemsgd@gmail.com>
2026-06-09 11:47:50 +0100

    feat: integrate Sentry error monitoring and performance tracing into Next.js application

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,11 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN || "your_dsn_here",
+  dsn: "https://1af752adb5cab09abd6e4e3e46ca1fc4@o4511535007006720.ingest.de.sentry.io/4511535020703825",
 
-  // Adjust this value in production, or use tracesSampler for greater control
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
 });
