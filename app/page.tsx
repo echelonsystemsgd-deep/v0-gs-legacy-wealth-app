d804d82fb036bac968195209745a3f26843abbc3
echelonsystemsgd-deep <echelonsystemsgd@gmail.com>
2026-08-15 20:30:01 +0100

    feat: initialize brand assets configuration and application home page

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default async function Home() {
                 "logo": {
                   "@type": "ImageObject",
                   "@id": "https://mercianwealth.com/#logo",
-                  "url": "https://mercianwealth.com/MercianWealthlogo.png",
+                  "url": "https://mercianwealth.com/MercianWealthLogo.jpeg",
                   "caption": "Mercian Wealth"
                 },
                 "image": {
